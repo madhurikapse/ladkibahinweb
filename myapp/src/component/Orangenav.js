8d13ef1b074e9cb843218c4a0f54a6200e286f37
madhurikapse <120490305+madhurikapse@users.noreply.github.com>
2024-08-12 21:56:23 +0530

    updates

---
+++ myapp/src/component/Orangenav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./orange.css"
+function Orangenav() {
+  return (
+    <div className='orange'>
+      
+    </div>
+  )
+}
+
+export default Orangenav
---
+++ myapp/src/component/navabr.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./navbar.css"
+function navabr() {
+  return (
+    <div>
+        <div className='navbar'>
+      <h5> हेल्पलाइन टोल फ्री संपर्क क्रमांक : १८१</h5>
+      <span><a href='@'>त्रुटी</a></span>
+      </div>
+      <div className='imgtag'>
+        <img src='https://ladakibahin.maharashtra.gov.in/new-logo.png'></img>
+        <img src='https://ladakibahin.maharashtra.gov.in/logo-maha.png'></img>
+        <div className='img2'>
+        <img src='https://ladakibahin.maharashtra.gov.in/govEmblem.png'></img>
+        </div>
+        
+      </div>
+          </div>
+  )
+}
+
+export default navabr
